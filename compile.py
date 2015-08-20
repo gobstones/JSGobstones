ab967ce95f0efd61136869427839395d85532ade
Ary Pablo Batista <arypbatista@gmail.com>
2015-08-19 21:39:44 -0300

    Added installation commando to compile.py

--- compile.py
+++ compile.py
@@ -2,16 +2,16 @@
 # Author: Ary Pablo Batista <arypbatista@gmail.com>
 """
     This file is part of JSGobstones.
-    JSGobstones is free software: you can redistribute it and/or 
-    modify it under the terms of the GNU General Public License as published 
+    JSGobstones is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published
     by the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    Foobar is distributed in the hope that it will be useful,
+    JSGobstones is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    along with JSGobstones.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os, os.path
@@ -23,26 +23,26 @@ class CmdApplication(object):
         self.input_key = _Getch()
         self.options = None
         self.arguments = None
-        
+
     def usage(self):
         return "No usage info.\n"
-        
+
     def get_option_handlers(self):
         return {}
-        
-    def get_option_switches(self):        
+
+    def get_option_switches(self):
         return self.get_option_handlers().keys()
-        
+
     def error(self, message):
         sys.stderr.write(message)
-            
+
     def get_option_name(self, option_switch):
         return remove_no(normalize_option_name(option_switch.split(" ")[0]))
-            
+
     def start(self):
         self.initialize()
         self.arguments, self.options = get_options(self.get_option_switches())
-        
+
         options_processed = []
         for option_switch, option_handler in self.get_option_handlers().iteritems():
             option = self.get_option_name(option_switch)
@@ -51,9 +51,9 @@ class CmdApplication(object):
                     option_handler(*self.options[option])
                 else:
                     option_handler()
-        
+
                 options_processed += [option]
-            
+
         if len(options_processed) == 0:
             self.error(self.usage().replace('<CMDLINE>', sys.argv[0]))
             sys.exit(1)
@@ -67,10 +67,10 @@ def write_file(filename, content):
     f = open(filename, 'w')
     f.write(content)
     f.close()
-    
+
 def delete_file(filename):
     os.unlink(filename)
-    
+
 def read_file(filename):
     f = open(filename, "r")
     content = f.read()
@@ -113,26 +113,26 @@ def default_options(option_switches):
 def get_option_names(option_switches):
     return [option.split(' ')[0] for option in option_switches]
 
-def parse_options(option_switches, args, max_args=None):    
+def parse_options(option_switches, args, max_args=None):
     arguments = []
     option_names = get_option_names(option_switches)
     switches = [normalize_option_name(s) for s in option_switches]
     args = map(lambda arg: normalize_option_name(arg) if arg in option_names else arg, args)
-    opt = default_options(switches)    
+    opt = default_options(switches)
     i = 1
     n = len(args)
-        
+
     # all args
     while i < len(args):
-        o = None        
-        
+        o = None
+
         # select matching option
         for oi in switches:
-            oi = oi.split(' ')            
+            oi = oi.split(' ')
             if oi[0] == args[i]:
                 o = oi
                 break
-        
+
         # This is an argument, not an option
         if o is None:
             if len(arguments) == max_args:
@@ -143,11 +143,11 @@ def parse_options(option_switches, args, max_args=None):
 
         # Check if single-word option
         # sw = current option
-        sw = o[0]                
+        sw = o[0]
         if len(o) == 1:
-            if sw[:3] == 'no-': 
+            if sw[:3] == 'no-':
                 neg = True
-                sw = sw[3:]            
+                sw = sw[3:]
             else:
                 neg = False
             opt[sw] = not neg
@@ -213,53 +213,61 @@ def get_files_matching(directory, matching):
 
 
 class RunTypescript(CmdApplication):
-    
+
     def get_option_handlers(self):
         return {
             "--main X" : self.build,
             "--purge"  : self.purge,
             "--no-build-html" : None,
             "--no-clean" : None,
-            "--no-build-parser": None
+            "--no-build-parser": None,
+            "--install" : self.install
         }
-        
+
+    def install(self):
+        print "Installing JSGobstones"
+        os.system("npm install")
+        os.system("bower install")
+        os.system("sudo gem install sass")
+        print "Done"
+
     def purge(self):
         print "The Purge begins!"
         bin_dir = os.path.join(os.getcwd(), "bin")
         if os.path.exists(bin_dir):
-            shutil.rmtree(bin_dir)  
+            shutil.rmtree(bin_dir)
         print "Ready"
-            
+
     def build(self, mainfile, target="ES5"):
         print "Compiling %s" % (mainfile,)
         this_dir = os.path.dirname(__file__)
-        
+
         bin_dir = os.path.join(this_dir, "public", "jsgobstones")
-        
+
         if self.options["build-parser"]:
             print "Compiling parser"
             os.system("pegjs -e \"var parser\" ./src/parser/GobstonesTranspiler.pegjs ./src/parser/GobstonesTranspiler.js".replace("\./", this_dir))
-        
+
         print "Building macro's file"
         os.system(os.path.join(this_dir, "src", "compiler", "GenerateMacrosFile.py"))
-        
+
         output = os.path.join(bin_dir, os.path.basename(mainfile)[:-3] + ".js")
-        
+
         build_cmd = "tsc --sourcemap --target %s --out %s %s" % (target, output, mainfile)
 
         if self.options["clean"]:
             if os.path.exists(bin_dir):
-                shutil.rmtree(bin_dir)    
+                shutil.rmtree(bin_dir)
             os.mkdir(bin_dir)
-        
+
         os.system(build_cmd)
-            
-        jsfiles = get_files_matching(os.path.dirname(mainfile), "*.js")        
+
+        jsfiles = get_files_matching(os.path.dirname(mainfile), "*.js")
         jsfiles = filter(lambda f: not mainfile[:-3] + ".js" in f and not "/gui/" in f, jsfiles)
         for f in jsfiles:
-            os.system("mv %s %s" % (f, bin_dir))        
+            os.system("mv %s %s" % (f, bin_dir))
         #os.system("mv %s %s" % (mainfile[:-3]+".js", bin_dir))
-        
+
         os.system("gulp")
         print "Ready"
 
